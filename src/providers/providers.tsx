29b1fc7084cf1970651157bdecbfe3a2af7d6198
Jorge Ramos <v-jorramos@expediagroup.com>
2025-04-22 17:53:24 -0600

    feat: install React Query DevTools dependency

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,5 +12,10 @@ interface Props {
 }
 
 export const Providers: React.FC<Props> = ({ children }) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
